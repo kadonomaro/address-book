67ad8af76373fe408ea28d3acc8616e0bdfa5a18
kadonomaro <denis.afer@gmail.com>
2020-02-24 20:06:53 +0700

    feat: add test data

---
+++ src/components/ContactList.vue
@@ -0,0 +1,29 @@
+<template>
+	<div class="contact-list">
+		<div v-for="contact in contacts" :key="contact.id">
+			<div>{{ contact.name }}</div>
+			<div>{{ contact.phone }}</div>
+			<div>{{ contact.email }}</div>
+			<hr>
+		</div>
+	</div>
+</template>
+
+<script>
+import contacts from '@/test/contacts.json';
+
+export default {
+	name: 'contact-list',
+	data() {
+		return {
+			contacts: contacts
+		}
+	}
+}
+</script>
+
+<style lang="scss">
+	.contact-list {
+
+	}
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,16 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+		<contact-list />
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import ContactList from '@/components/ContactList.vue'
 
 export default {
   name: 'Home',
   components: {
-    HelloWorld
+		ContactList
   }
 }
 </script>
