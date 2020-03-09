0d65a55cab968bda951c0a440d62c11ae9da0beb
kadonomaro <denis.afer@gmail.com>
2020-03-09 13:38:16 +0700

    feat: add firebase

--- src/main.js
+++ src/main.js
@@ -4,6 +4,10 @@ import router from './router'
 import store from './store'
 import '@/style/common.scss'
 
+import firebase from 'firebase/app'
+import 'firebase/firestore'
+import 'firebase/auth'
+
 import phoneFilter from '@/filters/phone.filter';
 import contactInfoDirective from '@/directives/contact-info.directive';
 
@@ -11,6 +15,22 @@ Vue.filter('phone', phoneFilter);
 Vue.directive('contact-info', contactInfoDirective);
 
 Vue.config.productionTip = false
+
+const firebaseDevConfig = {
+  apiKey: process.env.VUE_APP_FIREBASE_API_KEY,
+  authDomain: process.env.VUE_APP_FIREBASE_AUTH_DOMAIN,
+  databaseURL: process.env.VUE_APP_FIREBASE_DATABASE_URL,
+  projectId: process.env.VUE_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.VUE_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.VUE_APP_FIREBASE_MESSAGE_SENDER_ID,
+  appId: '1:664834727086:web:a7dd48b4f891b8e38c213f',
+};
+
+firebase.initializeApp(firebaseDevConfig);
+const db = firebase.firestore();
+const storage = firebase.storage();
+const auth = firebase.auth();
+
 
 new Vue({
   router,
