0574fdbae0acb91c30a1893d81c7729ba25d71c7
kadonomaro <denis.afer@gmail.com>
2020-03-20 20:20:58 +0700

    fix: remove contact-info directive duplicate info

--- src/directives/contact-info.directive.js
+++ src/directives/contact-info.directive.js
@@ -8,34 +8,16 @@ export default {
 		el.addEventListener('mouseenter', createTooltip);
 		el.addEventListener('mouseleave', removeTooltip);
 
-
-    var s = JSON.stringify
-    // el.innerHTML =
-    //   'value: '      + s(binding.value) + '<br>' +
-    //   'expression: ' + s(binding.expression) + '<br>' +
-    //   'argument: '   + s(binding.arg) + '<br>' +
-		//   'modifiers: '  + s(binding.modifiers) + '<br>'
-
 		function createTooltip(evt) {
 			if (!isCreated) {
 				contactInfo.style.left = evt.offsetX + 'px';
 				contactInfo.classList.add('contact-info');
 				contactInfo.innerHTML = `
-					<header class="contact-info__header">
-						<div class="contact-info__avatar">
-							<img src="${userDefaultAvatar}" alt="${binding.value.name}">
-						</div>
-						<div class="contact-info__user">
-							<span class="name"><b>Имя:</b> ${binding.value.name}</span>
-							<span class="sex"><b>Пол:</b> ${binding.value.sex}</span>
-							<span class="age"><b>Возраст:</b> ${binding.value.age}</span>
-						</div>
-					</header>
-					<footer class="contact-info__footer">
+					<div class="contact-info__inner">
 						<a href="tel:${binding.value.phone}" class="button call" title="Позвонить"></a>
 						<a href="mailto:${binding.value.email}" class="button send" title="Написать"></a>
 						<button class="button open" title="Подробнее"></button>
-					</footer>
+					</div>
 				`
 
 				vnode.elm.appendChild(contactInfo);
